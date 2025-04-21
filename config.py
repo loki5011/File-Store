93d22d8ae2ed443a06b738f42ce6b162875a0f9f
solo625 <wrio202365@gmail.com>
2025-04-22 01:59:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 import re
 import os
 from os import environ
@@ -20,11 +15,11 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "20714788"))
 API_HASH = environ.get("API_HASH", "25103f6394df6328a3ac776bf83f0e26")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7905144255:AAFOUBAa60ndAaS7rRmyKlD12gG0tJjIDmg")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7661941403:AAGp-YoYVosxgZUYb0wrhVo2FvTFD1LK-tI")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://i.ibb.co/rK3cz0Tk/photo-2025-04-20-17-15-52-7495445331476217860.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7009595359').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "robin_filestore_bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "mikasa_filestore_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -35,11 +30,11 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://Solo_65:ZtsFjTZYFB561Tns@cluster0.oggaj4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "Solo_65")
+DB_URI = environ.get("DB_URI", "mongodb+srv://deepandhinesh0:4DwxtL8aBb5jw4PS@cluster0.pkw8tje.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "deepandhinesh0")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "60")) # Time in Minutes
@@ -53,7 +48,7 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
 # Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
@@ -81,9 +76,4 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 URL = environ.get("URL", "")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
     
